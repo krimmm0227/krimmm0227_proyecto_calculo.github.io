da747fbef7b3bb97804a3edc91d96f1d58757bab
krimmm0227 <karimalemm2@gmail.com>
2024-10-26 23:51:03 -0700

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+var menu = document.getElementById("menu_ham");
+var boton = document.getElementById("boton");
+
+function cambio() {
+    /*boton.removeAttribute("class");
+    setTimeout(() => {
+        if (boton.innerHTML == "+") {
+            var cambio = "btn_menos";
+            boton.innerHTML = "-";
+            boton.classList.toggle(cambio);
+        } else {
+            var cambio = "btn_mas";
+            boton.innerHTML = "+";
+            boton.classList.toggle(cambio);
+        }
+        
+    }, 1);*/
+    boton.classList.toggle("btn_menos");
+    setTimeout(() => {
+        
+        menu.classList.toggle("menu_on");
+    }, 2);
+
+}
+
+function menu_off() {
+    menu.classList.toggle("menu_on");
+}
